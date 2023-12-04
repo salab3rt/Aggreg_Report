90f03541e2a6e04a57a743850935d8614e4314d3
salab3rt <salabert57@gmail.com>
2023-12-04 21:54:05 +0000

    -

--- Agreg.py
+++ Agreg.py
@@ -4,12 +4,8 @@ from pathlib import Path
 from datetime import datetime
 import re
 
-# Open the PDF
 pdf_folder = "./samples/"
 path = Path(pdf_folder)
-
-
-
 
 def report_stiching(file, backup_folder):
 
@@ -17,38 +13,34 @@ def report_stiching(file, backup_folder):
 
     pdf = fitz.open(file)
 
-    # Select a specific page (e.g., page 0)
     page = pdf.load_page(0)
     dpi=100
     image = page.get_pixmap(dpi=dpi, alpha=False)
     pdf.close()
-    #get the image from fitz
+    
+    # get the image from fitz
     pil_image = Image.frombytes("RGB", [image.width, image.height], image.samples)
 
-    #crop sections coords
+    # crop sections coords
     section_1_coords = (34, 31, 163, 50)
     section_2_coords = (34, 162, 579, 245)
     section_3_coords = (34, 352, 789, 781)
 
-    #crop
+    # crop
     section_1_image = pil_image.crop(section_1_coords)
     section_2_image = pil_image.crop(section_2_coords)
     section_3_image = pil_image.crop(section_3_coords)
 
+    # recrop one of the sections, to remove irrelevant info
     recrop_section_2_coords = (0, 0, 456, 15)
     section_2_width, section_2_height = section_2_image.size
 
-    #section_2_recrop = pil_image.crop(recrop_section_2_coords)
-
+    # create a new blank image and paste a blank section recrop on top of the section
     new_section_2_image = Image.new("RGB", (section_2_width, section_2_height), (255, 255, 255))
     new_section_2_image.paste(section_2_image, (0, 0))
     new_section_2_image.paste((255, 255, 255), recrop_section_2_coords)
 
-    #section_1_image.save("section1.jpg")
-    #new_section_2_image.save("section2.jpg")
-    #section_3_image.save("section3.jpg")
-
-
+    # create a new image from the cropped region
     new_image_size = (800, 520)
     new_image = Image.new("RGB", new_image_size, (255, 255, 255))
 
@@ -59,13 +51,9 @@ def report_stiching(file, backup_folder):
     new_image.paste(new_section_2_image, (45, 5))
     new_image.paste(section_3_image, (20, 85))
 
-    # Create a new image from the cropped region
-    #image.save("cropped_image.png")
 
-    # Define a regular expression pattern to match underscores or hyphens
+    # regular expression to get folder name from file
     pattern = r'[ _-]'
-
-    # Use the regular expression pattern to split the file name
     folder_name = re.split(pattern, file_name)
 
     current_year = datetime.today().year
